026c107fea348da10188d1d887b7ded3831a79d3
Yuku TAKAHASHI <taka84u9@gmail.com>
2018-09-03 17:07:10 +0900

    Implement

--- lib/active_interactor.rb
+++ lib/active_interactor.rb
@@ -1,5 +1,190 @@
+require "active_support"
+require "active_model"
+
 require "active_interactor/version"
 
 module ActiveInteractor
-  # Your code goes here...
+  extend ActiveSupport::Concern
+
+  class_methods do
+    # Configure {.validator_class}
+    #
+    # @param attribute_names [Array<Symbol>] list of
+    # @yield
+    # @return [void]
+    def validations(*attribute_names, &block)
+      @validation_attribute_names = attribute_names
+      validator_class.class_eval do
+        attr_accessor(*attribute_names)
+      end
+      validator_class.class_eval(&block)
+    end
+
+    # @return [Class]
+    def validator_class
+      @validator_class ||= build_validator_class
+    end
+
+    # Expose local instance variables into the returning value of {#call}
+    #
+    # @param instance_variable_names [Array<Symbol>]
+    def expose(*instance_variable_names)
+      instance_variable_names.each do |name|
+        exposures[name.to_sym] = "@#{name}"
+      end
+    end
+
+    # @return [Hash]
+    def exposures
+      @exposures ||= {}
+    end
+
+    # @return [Array<Symbol>]
+    def validation_attribute_names
+      @validation_attribute_names || []
+    end
+
+    # @return [Boolean]
+    def validation_required?
+      validation_attribute_names.present?
+    end
+
+    private
+
+    # @return [Class]
+    def build_validator_class
+      klass = Class.new do
+        include ActiveModel::AttributeAssignment
+        include ActiveModel::Validations
+        extend ActiveModel::Translation
+
+        # @note {ActiveModel::Translation} expects anonymous class to implement {.name}
+        cattr_accessor :name
+
+        # @note Override {ActiveModel::Translation::ClassMethods#i18n_scope}
+        def self.i18n_scope
+          :active_interactor
+        end
+      end
+      klass.name = name || 'ActiveInteractor' # Fallback for anonymous classes
+      klass
+    end
+  end
+
+  included do
+    prepend ActiveInteractor::Interface
+    attr_reader :errors
+  end
+
+  # Interactor interface.
+  module Interface
+    # @param args [Array<(nil)>, Array<(Hash)>]
+    # @return [ActiveInteractor::Result]
+    def call(*args)
+      fail ArgumentError if args.size > 1
+      fail ArgumentError if args.size == 1 && !args.first.is_a?(Hash)
+
+      params = args.extract_options!
+      validator = self.class.validator_class.new
+
+      if params.empty? && !self.class.validation_required?
+        @errors = validator.errors
+        super
+      else
+        params = sanitize(params)
+        @errors = validate(params)
+        super(params) if @errors.empty?
+      end
+
+      Result.new(result_payload, @errors)
+    end
+  end
+
+  # Result of an operation.
+  class Result
+    # @return [ActiveModel::Errors]
+    attr_reader :errors
+
+    # Concrete methods
+    #
+    # @see ActiveInteractor::Result#respond_to_missing?
+    METHODS = Set.new([:initialize, :success?, :failure?])
+
+    # @param payload [Hash]
+    # @param errors [ActiveModel::Errors]
+    def initialize(payload, errors)
+      @payload = payload.symbolize_keys
+      @errors = errors
+    end
+
+    # @return [Boolean]
+    def success?
+      !failure?
+    end
+
+    # @return [Boolean]
+    def failure?
+      errors.present?
+    end
+
+    private
+
+    def method_missing(method_name, *)
+      @payload.fetch(method_name) { super }
+    end
+
+    def respond_to_missing?(method_name, _include_all)
+      method_name = method_name.to_sym
+      METHODS.include?(method_name) || @payload.key?(method_name)
+    end
+  end
+
+  def call(*)
+    fail NotImplementedError
+  end
+
+  # Merge the given errors into {#errors}.
+  #
+  # @param additional_errors [ActiveModel::Errors]
+  # @return [void]
+  def merge_errors(additional_errors)
+    additional_errors.full_messages.each do |message|
+      errors.add(:base, message)
+    end
+  end
+
+  private # rubocop:disable Lint/UselessAccessModifier
+
+  # @return [Hash] a hash representing a payload for {ActiveInteractor::Result}
+  def result_payload
+    Hash[].tap do |result|
+      self.class.exposures.each do |name, ivar|
+        result[name] = instance_variable_defined?(ivar) ? instance_variable_get(ivar) : nil
+      end
+    end
+  end
+
+  # @param args [Hash]
+  # @return [ActiveModel::Errors]
+  def validate(params)
+    validator = self.class.validator_class.new
+    if self.class.validation_required?
+      validator.assign_attributes(params)
+      validator.valid?
+    end
+    validator.errors
+  end
+
+  # Remove undeclared keys from the given params.
+  #
+  # @param params [Hash]
+  # @return [Hash]
+  def sanitize(params)
+    return params unless self.class.validation_required?
+    Hash[].tap do |result|
+      self.class.validation_attribute_names.each do |name|
+        result[name] = params[name]
+      end
+    end
+  end
 end
--- spec/active_interactor_spec.rb
+++ spec/active_interactor_spec.rb
@@ -1,9 +1,117 @@
 RSpec.describe ActiveInteractor do
-  it "has a version number" do
-    expect(ActiveInteractor::VERSION).not_to be nil
+  describe 'interactor with validations' do
+    subject { interactor.call(*params) }
+
+    let(:interactor_class) do
+      Class.new do
+        include ActiveInteractor
+
+        expose :product
+
+        validations(:name, :price) do
+          validates :name, presence: true, length: { maximum: 50 }
+          validates :price, presence: true, numericality: { only_integer: true }
+        end
+
+        def initialize(repository)
+          @repository = repository
+        end
+
+        def call(attributes)
+          @product = @repository.create(attributes)
+        end
+      end
+    end
+
+    let(:interactor) { interactor_class.new(repository) }
+    let(:repository) { double('ProductRepository', create: created_product) }
+    let(:created_product) { Object.new }
+
+    context 'with valid params' do
+      let(:params) { [{ name: 'Qiitan', price: 100 }] }
+
+      it 'returns a successful result with exposing the product' do
+        is_expected.to be_success.and have_attributes(product: created_product)
+      end
+    end
+
+    context 'with malicious param' do
+      let(:params) { [{ name: 'Qiitan', price: 100, malicious: 'param' }] }
+
+      it "doesn't pass through the malicious param to a repository" do
+        expect(subject).to be_success
+        expect(repository).to have_received(:create).with(name: 'Qiitan', price: 100)
+      end
+    end
+
+    context 'with invalid params' do
+      let(:params) { [{ name: 'Product without price' }] }
+
+      it 'returns a failure result with errors' do
+        is_expected.to be_failure.and have_attributes(
+          errors: an_object_having_attributes(
+            full_messages: match_array(["Price can't be blank", 'Price is not a number'])
+          )
+        )
+      end
+    end
+
+    context 'with non-hash params' do
+      let(:params) { ['string parameter'] }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
+
+    context 'with two hash params' do
+      let(:params) { [{ name: 'Qiitan' }, { price: 100 }] }
+      it { expect { subject }.to raise_error(ArgumentError) }
+    end
   end
 
-  it "does something useful" do
-    expect(false).to eq(true)
+  example 'without validations' do
+    interactor_class = Class.new do
+      include ActiveInteractor
+
+      expose :product
+
+      def call(value:)
+        @product = value * 2
+      end
+    end
+
+    result = interactor_class.new.call(value: 10)
+    expect(result).to be_success.and(have_attributes(product: 20))
+  end
+
+  example 'fail while calling' do
+    interactor_class = Class.new do
+      include ActiveInteractor
+
+      expose :product
+
+      def call
+        errors.add(:base, :invalid)
+      end
+    end
+
+    result = interactor_class.new.call
+    expect(result).to be_failure.and(have_attributes(product: nil))
+  end
+
+  example 'using #merge_errors' do
+    interactor_class = Class.new do
+      include ActiveInteractor
+
+      def initialize(errors)
+        @additional_errors = errors
+      end
+
+      def call
+        merge_errors(@additional_errors)
+      end
+    end
+
+    result = interactor_class.new(instance_double('ActiveModel::Errors', full_messages: ['custom error message'])).call
+    expect(result).to be_failure
+    expect(result.errors.full_messages).to eq ['custom error message']
   end
 end
